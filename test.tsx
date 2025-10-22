08bed083711179819c37e1976f10ad0eec4a3962
geoffsee <>
2025-10-22 18:48:27 -0400

    rename source to src

--- test.tsx
+++ test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import chalk from 'chalk';
 import test from 'ava';
 import {render} from 'ink-testing-library';
-import App from './source/app.tsx';
+import App from './src/app.tsx';
 
 test('greet unknown user', t => {
 	const {lastFrame} = render(<App name={undefined} />);
