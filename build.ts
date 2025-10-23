a773671d010eedfc57fa4bdb191bd532e528e8ea
geoffsee <>
2025-10-23 12:14:30 -0400

    - Bump version to `0.1.2`. - Migrate dependencies to `devDependencies`. - Update build script to include `yoga.wasm`.

--- build.ts
+++ build.ts
@@ -2,6 +2,11 @@ await Bun.build({
 	entrypoints: ['./src/cli.tsx'],
 	outdir: './dist',
 	target: 'node',
-	external: ['react-devtools-core'],
+	format: 'esm',
 	minify: true,
+	// external: ['react-devtools-core'],
+	// splitting: true
 });
+
+// Copy yoga.wasm to dist folder
+await Bun.write('./dist/yoga.wasm', Bun.file('./node_modules/yoga-wasm-web/dist/yoga.wasm'));
